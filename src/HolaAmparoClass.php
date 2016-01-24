53748a8b5bb6bc970c067a864778a1a63acb5144
hopita <amparomegias@hotmail.com>
2016-01-24 01:49:40 +0100

    inicio

---
+++ src/HolaAmparoClass.php
@@ -0,0 +1,19 @@
+<?php
+namespace Hopita\Composer;
+/**
+ * A terribly useful class
+ *
+ * See: http://knpuniversity.com/screencast/question-answer-day
+ */
+class HolaAmparoClass
+{
+	public function __construct()
+    {
+        
+    }
+
+    public function sayHallo()
+    {
+        return 'Hallo!';
+    }
+}
